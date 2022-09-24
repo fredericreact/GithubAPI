05acb51166a9045e566db69a71a3b6a5ea656801
unknown <fredericreact@gmail.com>
2022-09-24 11:35:02 +0800

    add props to component message

--- README.md
+++ README.md
@@ -8,3 +8,17 @@ $  yarn add semantic-ui-react semantic-ui-css
 
 utiliser import car webpack va tout deplacer
 
+# PropTypes
+
+```javascript 
+Message.defaultProps ={
+    total:0
+}
+
+Message.propTypes = {
+    total : PropTypes.number
+}
+
+```
+
+Je peux definir en not required et fournir une valeur par defaut
--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -3,11 +3,20 @@ import PropTypes from 'prop-types';
 import './style.scss';
 import {Message as MessageSemantic} from 'semantic-ui-react'
 
-const Message =() =>{
+const Message =({total}) =>{
+    const message =total <=1 ? `${total} resultat` : `${total} results`;
     return (
 
-        <MessageSemantic content="la recherche a donne"/>
+        <MessageSemantic content={`la recherche a donnee ${message}`}/>
     )
+}
+
+Message.defaultProps ={
+    total:0
+}
+
+Message.propTypes = {
+    total : PropTypes.number
 }
 
 export default Message;
